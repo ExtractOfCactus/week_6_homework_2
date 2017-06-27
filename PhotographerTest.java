e24dfa878fd46f29aacc9f083deddc5c59a9a489
Glen Young <extractofcactus@icloud.com>
2017-06-27 18:37:17 +0100

    instances of new classes used in tests

--- PhotographerTest.java
+++ PhotographerTest.java
@@ -4,12 +4,18 @@ import org.junit.*;
 public class PhotographerTest{
 
   Photographer photographer;
-  Camera camera;
+  AnalogCamera analogCamera1;
+  AnalogCamera analogCamera2;
+  DigitalCamera digitalCamera1;
+  DigitalCamera digitalCamera2;
 
   @Before
   public void before() {
     photographer = new Photographer("Frank");
-    camera = new Camera();
+    analogCamera1 = new AnalogCamera("Kodak, plastic and disposable");
+    analogCamera2 = new AnalogCamera("Evian, red-eye removal, disposable");
+    digitalCamera1 = new DigitalCamera("Nikon, sharp focus, high-end");
+    digitalCamera2 = new DigitalCamera("Fukishima, shake-elimination, paneramic abilites");
   }
 
   @Test
@@ -24,14 +30,14 @@ public class PhotographerTest{
 
   @Test
   public void canAddCamera() {
-    photographer.addCamera(camera);
+    photographer.addCamera(analogCamera1);
     assertEquals(1, photographer.camerasCount());
   }
 
   @Test
   public void canRemoveCamera() {
-    photographer.addCamera(camera);
-    photographer.addCamera(camera);
+    photographer.addCamera(analogCamera2);
+    photographer.addCamera(digitalCamera1);
     photographer.removeCamera();
     assertEquals(1, photographer.camerasCount());
   }
